36241df2a7bdd9e8de048efdc143e09596670f1f
Work <work@xavier-zenbookpro>
2019-01-19 11:07:45 +0100

    Added missing XPaths

--- xpaths.js
+++ xpaths.js
@@ -3,4 +3,9 @@ module.exports = {
     email: "//p[@itemprop='email']",
     website: "//li[contains(div, 'Site Web')]//a",
     phone: "//p[@itemprop='telephone']",
+    employees: "//p[@itemprop='numberOfEmployees']",
+    address: "//p[@itemprop='address']/text()",
+    founders: "//div[@itemprop='founders']/p/text()",
+    markets: "//h2[@class='txtArtTitre'][.='Marché :']/following-sibling::div[1]/p/text()",
+    creationDate: "//meta[@itemprop='foundingDate']/following-sibling::p[1]",
 };
